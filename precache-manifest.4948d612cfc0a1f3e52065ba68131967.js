2536b69bb4a9bba28060eb91b99a2bfd99c54975
Kim Yong <kimyong.lim.2017@gmail.com>
2019-07-17 22:01:11 +0800

    Updates

---
+++ precache-manifest.4948d612cfc0a1f3e52065ba68131967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5214c3711f88afac8d75dd2063934c8c",
+    "url": "/simpleVideoSearch/index.html"
+  },
+  {
+    "revision": "42f5c0a8e745990cc66b",
+    "url": "/simpleVideoSearch/static/css/main.98133e51.chunk.css"
+  },
+  {
+    "revision": "60e8b962508740592b39",
+    "url": "/simpleVideoSearch/static/js/2.60cf27c0.chunk.js"
+  },
+  {
+    "revision": "42f5c0a8e745990cc66b",
+    "url": "/simpleVideoSearch/static/js/main.f027bdc7.chunk.js"
+  },
+  {
+    "revision": "34630e523e8a0cdb488d",
+    "url": "/simpleVideoSearch/static/js/runtime~main.c0d346a8.js"
+  }
+]);
